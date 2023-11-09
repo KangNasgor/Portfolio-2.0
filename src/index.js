ead3f051704eae5f711e5be5da02d93f25736f1d
KangLeonard <74401836+KangNasgor@users.noreply.github.com>
2023-11-09 11:11:04 +0700

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router } from 'react-router-dom';
+import App from './App';
+const app = ReactDOM.createRoot(document.getElementById('mainmenu'));
+app.render(<App />);
+
